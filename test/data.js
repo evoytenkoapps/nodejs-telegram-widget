bd8d8db9d44111d325ac30e55ed508b527b5cb43
Евгений Войтенко <jozik.service@gmail.com>
2020-11-11 12:02:03 +0300

    add packa

---
+++ test/data.js
@@ -0,0 +1,10 @@
+module.exports = {
+  auth_date: 1605084715,
+  first_name: "Евгений",
+  hash: "bf476ea907781c3f6a70409be9bf2ff458aa06ec5d7ec5e3f41b79796c762044",
+  id: 287853668,
+  last_name: "Войтенко",
+  photo_url:
+    "https://t.me/i/userpic/320/kI908OesMrj9S3OPiLJy26FwxMKdVHmxSLzej5zCFBo.jpg",
+  username: "Evgeny_Voytenko"
+};
